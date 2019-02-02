0ccd47cb76d426056be4924f727c9559d21049f1
Afes Oktavianus <afes,.okta@gmail.com>
2019-02-02 19:10:52 +0700

    - perbaikan untuk crud jemaat - tambahkan method untuk mendapatkan kode jemaat secara increment berdasarkan   tgl

--- src/main/java/com/id/sigada/controller/pendataan/JemaatController.java
+++ src/main/java/com/id/sigada/controller/pendataan/JemaatController.java
@@ -11,13 +11,20 @@ import com.id.sigada.dao.KepelDao;
 import com.id.sigada.dao.KomisiDao;
 import com.id.sigada.entities.RmKel;
 import com.id.sigada.entities.RpJmt;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
 
 /**
  *
@@ -88,4 +95,44 @@ public class JemaatController {
         model.addAttribute("listKomisi", komisiDao.findAll());
     }
     
+    @PostMapping("/jemaat/formAll")
+    public String prosesForm(@ModelAttribute @Valid RpJmt jemaat, BindingResult errors, SessionStatus status) {
+        if (errors.hasErrors()) {
+            return "jemaat/formAll";
+        } 
+        if (jemaat.getJmtgl()!=null) {
+            jemaat.setJmkod(getMaxKlkod(jemaat.getJmtgl()));
+        }else {
+            return "jemaat/formAll";
+        }
+        dao.save(jemaat);
+        status.setComplete();
+        return "redirect:list";
+    }
+    /**
+     * 
+     * @return Max Kode Jemaat
+     */
+    private String getMaxKlkod(Date tgl) {
+        String id;
+        SimpleDateFormat sd = new SimpleDateFormat("yyyy-mm-dd");
+        SimpleDateFormat sdf = new SimpleDateFormat("yymmdd");
+        String maxKode = dao.getMaxKode(sd.format(tgl));        
+        if ("0".equals(maxKode)) {
+            return "01";
+        } else {
+            id = String.valueOf(Integer.valueOf(maxKode) + 1);
+        }
+        String comCod = "";
+        switch (id.length()) {
+            case 1:
+                id = "0" + id;
+                break;
+            case 2:
+                id = id;
+                break;                        
+        }
+        comCod = sdf.format(tgl).concat(id);
+        return comCod;
+    }
 }
--- src/main/java/com/id/sigada/dao/JemaatDao.java
+++ src/main/java/com/id/sigada/dao/JemaatDao.java
@@ -6,6 +6,7 @@
 package com.id.sigada.dao;
 
 import com.id.sigada.entities.RpJmt;
+import java.util.Date;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,4 +27,7 @@ public interface JemaatDao extends JpaRepository<RpJmt, String>{
     Page<RpJmt> findByJmnamContainingIgnoreCase2(@Param("klkod") String klkod,@Param("jmnam")  String jmnam, Pageable pageable);
     
     Page<RpJmt> findByJmnamContainingIgnoreCase(String jmnam, Pageable pageable);
+    
+    @Query(value="SELECT COALESCE( NULLIF(Max(right(jmkod,2)),''), '0' ) FROM RpJmt r WHERE r.jmtgl = :jmtgl",nativeQuery = true )
+    public String getMaxKode(@Param("jmtgl")  String jmtgl);
 }
